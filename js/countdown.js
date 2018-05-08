aa5d9743ed5e1c9f1970a38e46c238d44d41b3c4
jasmine <jasminewiggins90@yahoo.com>
2018-05-07 19:09:21 -0500

    Fixed countdown date

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  var clock = $('.your-clock').FlipClock( 3600 * 24 * 372,{
+  var today = new Date();
+  var wedding_day = new Date(2019, 04, 04);
+
+  var one_day = 1000*60*60*24;
+  var days_until = Math.ceil((wedding_day.getTime()-today.getTime())/(one_day));
+
+  var clock = $('.your-clock').FlipClock( 3600 * 24 * days_until,{
       clockFace: 'DailyCounter',
       countdown: true
   });
